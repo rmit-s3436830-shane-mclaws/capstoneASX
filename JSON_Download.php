477e5b51ff245c3eae26c24fb857d812c8cf037b
Callum Pullyblank <s3378543@student.rmit.edu.au>
2017-03-17 09:31:51 +1100

    Add files via upload
    +++Added additional flags on data retrieval, more information for potential use+++
    ***Changed system so that it creates a new file at start of day and uploads data to it, additional data pulls are the appended to end of the day's file. Means that rather than have 21 files/listing/day, there's 1 file/listing/day, and each file has 21 entries***

--- JSON_Download.php
+++ JSON_Download.php
@@ -3,17 +3,9 @@
 	* This script will grab a list of trading shares from the ASX website. *
 	* It will then grab the latest JSON file for each share and upload the *
 	* JSON to the S3 bucket for later use. This script will be run every   *
-	* 20 minutes during ASX trading hours, as automated by the EC2 OS the  *
-	* script will be hosted on.                                            *
+	* 20 minutes during ASX trading hours (10AM - 5PM), as automated by    *
+	* the EC2 OS the script will be hosted on.                             *
 	***********************************************************************/
-	
-	//Used for checking link validity
-	function get_http_response_code($url)
-	{
-		$headers = get_headers($url);
-		return substr($headers[0], 9, 3);
-	}
-	/*****************************************************/
 	
 	//import library used to interface with S3
 	require 'vendor/autoload.php';
@@ -81,30 +73,74 @@
 	{
 		$tries++;
 			//echo $company.": data point#".$tries; //Used for debugging
-		$tags = "nt1l1c1p2ohg"; //The different tags correspond to different attributes of the stock entry. Explanation can be found at: http://www.marketindex.com.au/yahoo-finance-api
+		
+		//Retrieves the most up to date data from yahoo finance
+		$tags = "nabl1t1c1p2ohgpwkjdqr1y"; //The different tags correspond to different attributes of the stock entry. Explanation can be found at: http://www.marketindex.com.au/yahoo-finance-api
 		$dataURL = "http://finance.yahoo.com/d/quotes.csv?s=".$company.".AX&f=".$tags;
 		$data = file_get_contents($dataURL);
-		$fileName = $company."/".$date."-".$time.".json"; //Filename exists within folder 'ASX trading code', formatted as YYYYMMDD-HHMM.json, example: CBA/20170313-17:40.json would be CBA on the 13th of March 2017 at 5:40PM
+		
+		//Filename exists within folder 'ASX trading code', formatted as YYYYMMDD.json, example: CBA/20170313.json would be CBA on the 13th of March 2017
+		$fileName = $company."/".$date.".json"; 
+		
+		//Formats raw CSV into a readable format for alter use
 		$entry = explode(',', $data);
-		$entry = '{"Name":'.$entry[0].',"Last Trade Time":'.$entry[1].',"Last Trade Price":"'.$entry[2].'","Change":"'.$entry[3].'","Change(%)":'.$entry[4].',"Opening Value":"'.$entry[5].'","Day High":"'.$entry[6].'","Day Low":"'.substr($entry[7],0,-1).'"}'; //Formats raw CSV into a readable format
+		$entry = '{"Time":"'.$time.'","Name":'.$entry[0].',"ASX Code":"'.$company.'","Ask Price":"'.$entry[1].'","Bid Price":"'.$entry[2].'","Last Trade Price":"'.$entry[3].'","Last Trade Time":'.$entry[4].',"Change":"'.$entry[5].'","Change(%)":'.$entry[6].',"Opening Value":"'.$entry[7].'","Day High":"'.$entry[8].'","Day Low":"'.$entry[9].'","Previous Close":"'.$entry[10].'","52 Week Range":'.$entry[11].',"52 Week High":"'.$entry[12].'","52 Week Low":"'.$entry[13].'","Dividend/Share":"'.$entry[14].'","Ex-Dividend Date":"'.$entry[15].'","Dividende Pay Date":"'.$entry[16].'","Dividend Yield":"'.substr($entry[17],0,-1).'"}';
 			//echo " Entry: ".$entry; //Used for debugging
-		try
+		
+		if($time == "10:00") //If it's the first post of the day, create a new file and put the data in it
 		{
-			$s3Client->putObject(array(
-				'Bucket' => $AWS_Bucket_ID,
-				'Key' => $fileName,
-				'Body' => $entry
-			));
+			try
+			{
+				$s3Client->putObject(array(
+					'Bucket' => $AWS_Bucket_ID,
+					'Key' => $fileName,
+					'Body' => $entry
+				));
+			}
+			catch (S3Exception $e)
+			{
+				echo "Exception when creating new file\n";
+				echo $e->getMessage();
+				break;
+			}
 		}
-		catch (S3Exception $e)
+		else //If the day's file exists, open it and append to the end
 		{
-			echo $e->getMessage();
+			try
+			{
+				$result = $s3Client->getObject(array(
+					'Bucket' => $AWS_Bucket_ID,
+					'Key' => $fileName
+					));
+				$entry = $result['Body']."\n".$entry;
+					//echo $entry."\n"; //Used for debugging
+				try
+				{
+					$s3Client->putObject(array(
+						'Bucket' => $AWS_Bucket_ID,
+						'Key' => $fileName,
+						'Body' => $entry
+					));
+				}
+				catch (S3Exception $e)
+				{
+					echo "Exception when writing to existing file\n";
+					echo $e->getMessage();
+					break;
+				}
+			}
+			catch (S3Exception $e)
+			{
+				echo "Exception when opening existing file\n";
+				echo $e->getMessage();
+				break;
+			}
 		}
-		if($tries == 150) //This if will trigger once the first 150 entries have been grabbed, and then break out of the loop
+		/*if($tries == 150) //This if will trigger once the first 150 entries have been grabbed, and then break out of the loop
 		{ //Delete/comment this if block once debugging is complete and sprint is ready for presentation/review
 			break; //Deleting/commenting this block will allow all ~2000 entries to be computed
-		}
-			echo "\n"; //Used for debugging
+		}*/
+			//echo "\n"; //Used for debugging
 	}
 		//echo "\n".$tries." trading codes attempted.\n"; // Used for debugging
 	/*****************************************************/
