d616f2ff8f565dd464f51c21c96fd3f7490a8f52
Callum Pullyblank <s3378543@student.rmit.edu.au>
2017-03-18 00:16:23 +1100

    Add files via upload
    Script will now only download the company listing CSV from the ASX at 10:00AM (first run). During this process, it will generate a CSV containing only the ASX Company codes, and save this CSV locally. The Script from that point onward in the day will refer to the local CSV file for it's list of company codes, rather than download the ASX supplied CSV every time. 
    
    The ASX CSV gets updated daily at midnight, so to redownload it every 20 minutes and not actually save it anywhere was a bit of a waste.

--- JSON_Download.php
+++ JSON_Download.php
@@ -37,33 +37,59 @@
 	$time = date('H:i');
 	/*****************************************************/
 	
-	//Create an array of company trading codes
-	$ASX_Company_List = fopen("http://www.asx.com.au/asx/research/ASXListedCompanies.csv", "r"); //Perma-link to most up to date CSV from ASX
-	if(!$ASX_Company_List)
+	//Download an array of company trading codes to a local file at start of day
+	if($time == "10:00")
 	{
-		//Exit if failed to retrieve csv from ASX
-		echo "Unable to open remote file!\n";
-		exit;
-	}
-	$Companies = array();
-	while(($line = fgets($ASX_Company_List)) != false)
-	{
-		$elements = explode(',', $line);
-		//Check validity of line from file, if it contains a valid company entry
-		if(count($elements) >= 3)
+		$ASX_Company_List = fopen("http://www.asx.com.au/asx/research/ASXListedCompanies.csv", "r"); //Perma-link to most up to date CSV from ASX
+		if($ASX_Company_List) //If remote file fails to open, skip this code block and proceed to using the current local file
 		{
-			for($i=0; $i<count($elements); $i++)
+			$Codes = array();
+			while(($line = fgets($ASX_Company_List)) != false)
 			{
-				if(strlen($elements[$i]) == 3) //Check if section of line contains the company trading code
+				$elements = explode(',', $line);
+				//Check validity of line from file, if it contains a valid company entry
+				if(count($elements) >= 3)
 				{
-					array_push($Companies, $elements[$i]);
-					break;
+					for($i=0; $i<count($elements); $i++)
+					{
+						if(strlen($elements[$i]) == 3) //Check if section of line contains the company trading code
+						{
+							array_push($Codes, $elements[$i]);
+							break;
+						}
+					}
 				}
 			}
+			fclose($ASX_Company_List);
+			$Local_Company_List = fopen("/home/ec2-user/ASX_JSON/companies.csv","w");
+			foreach($Codes as $code)
+			{
+				if($code == reset($Codes)) //First iteration
+				{
+					fwrite($Local_Company_List, $code);
+				}
+				else
+				{
+					fwrite($Local_Company_List, ",".$code);
+				}
+			}
+			fclose($Local_Company_List);
 		}
 	}
-	//print_r($Companies);
-	fclose($ASX_Company_List);
+	/*****************************************************/
+	
+	//Grab the array of company codes from the local file
+	$Local_Company_List = fopen("/home/ec2-user/ASX_JSON/companies.csv", "r");
+	if(!$Local_Company_List)
+	{
+		echo "Unable to open local file!\n";
+		echo "Exiting...\n";
+		exit;
+	}
+	$line = fgets($Local_Company_List);
+	$Companies = explode(',', $line);
+	fclose($Local_Company_List);
+	
 	/*****************************************************/
 	
 	//Grabs the latest JSON for each company listed and saves it to the S3 bucket
@@ -72,20 +98,17 @@
 	foreach($Companies as $company)
 	{
 		$tries++;
-			//echo $company.": data point#".$tries; //Used for debugging
-		
 		//Retrieves the most up to date data from yahoo finance
 		$tags = "nabl1t1c1p2ohgpwkjdqr1y"; //The different tags correspond to different attributes of the stock entry. Explanation can be found at: http://www.marketindex.com.au/yahoo-finance-api
 		$dataURL = "http://finance.yahoo.com/d/quotes.csv?s=".$company.".AX&f=".$tags;
 		$data = file_get_contents($dataURL);
 		
 		//Filename exists within folder 'ASX trading code', formatted as YYYYMMDD.json, example: CBA/20170313.json would be CBA on the 13th of March 2017
-		$fileName = $company."/".$date.".json"; 
+		$fileName = $company."/".$date.".json";	
 		
 		//Formats raw CSV into a readable format for alter use
 		$entry = explode(',', $data);
 		$entry = '{"Time":"'.$time.'","Name":'.$entry[0].',"ASX Code":"'.$company.'","Ask Price":"'.$entry[1].'","Bid Price":"'.$entry[2].'","Last Trade Price":"'.$entry[3].'","Last Trade Time":'.$entry[4].',"Change":"'.$entry[5].'","Change(%)":'.$entry[6].',"Opening Value":"'.$entry[7].'","Day High":"'.$entry[8].'","Day Low":"'.$entry[9].'","Previous Close":"'.$entry[10].'","52 Week Range":'.$entry[11].',"52 Week High":"'.$entry[12].'","52 Week Low":"'.$entry[13].'","Dividend/Share":"'.$entry[14].'","Ex-Dividend Date":"'.$entry[15].'","Dividende Pay Date":"'.$entry[16].'","Dividend Yield":"'.substr($entry[17],0,-1).'"}';
-			//echo " Entry: ".$entry; //Used for debugging
 		
 		if($time == "10:00") //If it's the first post of the day, create a new file and put the data in it
 		{
@@ -113,7 +136,6 @@
 					'Key' => $fileName
 					));
 				$entry = $result['Body']."\n".$entry;
-					//echo $entry."\n"; //Used for debugging
 				try
 				{
 					$s3Client->putObject(array(
@@ -140,8 +162,6 @@
 		{ //Delete/comment this if block once debugging is complete and sprint is ready for presentation/review
 			break; //Deleting/commenting this block will allow all ~2000 entries to be computed
 		}*/
-			//echo "\n"; //Used for debugging
 	}
-		//echo "\n".$tries." trading codes attempted.\n"; // Used for debugging
 	/*****************************************************/
 ?>
