fd110224925a98df0b05af2a5672532a020a3afb
Callum Pullyblank <s3378543@student.rmit.edu.au>
2017-05-01 22:44:58 +1000

    Altered Admin Menu functions
    Altered admin menu functions to use the new returnPlayerList function

--- FrontEnd/Game.java
+++ FrontEnd/Game.java
@@ -19,7 +19,8 @@ import java.util.zip.DeflaterOutputStream;
 
 import org.json.*;
 
-public class Game {
+public class Game
+{
 	float buyersFee = 50;
 	
 	// function for buying stocks
@@ -27,26 +28,35 @@ public class Game {
 	// add those stocks to the player, calculate and subtract money from balance
 	// and add the transaction to the players history
 	// returns TRUE or FALSE depending on if it could successfully buy the stocks
-	protected static boolean buyStocks(String asxCode, int number){
+	protected static boolean buyStocks(String asxCode, int number)
+	{
 		int index;
 		float price, totalPrice, totalPriceWithFee;
-		for (index = 0; index < AsxGame.stockArray.size(); index++){							
-			if (asxCode.equals(AsxGame.stockArray.get(index).code)){	//searches stockArray for stock
+		for (index = 0; index < AsxGame.stockArray.size(); index++)
+		{							
+			if (asxCode.equals(AsxGame.stockArray.get(index).code))
+			{	//searches stockArray for stock
 				price = AsxGame.stockArray.get(index).askPrice;			//gets stocks price
 				totalPrice = price * number;							//calculates the price of that many stocks
 				totalPriceWithFee = totalPrice + calcBrokersFeePurch(totalPrice); //calculates the total price including fee
 				
-				if (totalPriceWithFee <= AsxGame.activePlayer.balance){		//checks that player can afford it
+				if (totalPriceWithFee <= AsxGame.activePlayer.balance)
+				{		//checks that player can afford it
 					//add shares and update player
-					if (AsxGame.activePlayer.addShares(asxCode, number)){	
+					if (AsxGame.activePlayer.addShares(asxCode, number))
+					{	
 						AsxGame.activePlayer.removeBalance(totalPrice + calcBrokersFeePurch(totalPrice));
 						AsxGame.activePlayer.calcValue();
 						AsxGame.activePlayer.updateTransHist("-1", "-1", asxCode, "Buy", number, price);
 						return true;
-					} else {
+					}
+					else
+					{
 						return false;
 					}
-				} else {
+				}
+				else
+				{
 					System.out.println("Error: Insufficient Funds");
 					return false;
 				}
@@ -59,24 +69,32 @@ public class Game {
 	// pass an asxCode and number of stocks to sell and it will
 	// remove those stocks from the player, calculate and add money to balance
 	// and add the transaction to the players history
-	protected static boolean sellStocks(String asxCode, int number){
+	protected static boolean sellStocks(String asxCode, int number)
+	{
 		int index;
 		float price, totalPrice;
-		if (number <= AsxGame.activePlayer.getShareCount(asxCode)){
-			for (index = 0; index < AsxGame.stockArray.size(); index++){
-				if (asxCode.equals(AsxGame.stockArray.get(index).code)){
+		if (number <= AsxGame.activePlayer.getShareCount(asxCode))
+		{
+			for (index = 0; index < AsxGame.stockArray.size(); index++)
+			{
+				if (asxCode.equals(AsxGame.stockArray.get(index).code))
+				{
 					price = AsxGame.stockArray.get(index).askPrice;
 					int currentStocks = AsxGame.activePlayer.getShareCount(asxCode);
-					if (number > currentStocks){
+					if (number > currentStocks)
+					{
 						number = currentStocks;
 					}
 					totalPrice = price * number;
-					if (AsxGame.activePlayer.removeShares(asxCode, number)){
+					if (AsxGame.activePlayer.removeShares(asxCode, number))
+					{
 						AsxGame.activePlayer.addBalance(totalPrice - calcBrokersFeeSale(totalPrice));
 						AsxGame.activePlayer.calcValue();
 						AsxGame.activePlayer.updateTransHist("-1", "-1", asxCode, "Sell", number, price);
 						return true;
-					} else {
+					}
+					else
+					{
 						return false;
 					}
 				}
@@ -89,12 +107,14 @@ public class Game {
 	// pass in string and password
 	// if successfully logged in, will load player into AsxGame.activePlayer
 	// returns TRUE if login is successful, otherwise FALSE
-	protected static boolean login(String uEmail, String password){
+	protected static boolean login(String uEmail, String password)
+	{
 		Socket connection = null;
 		boolean successState = false;
 		long startTime = System.currentTimeMillis();
 		long currentTime, elapsedTime;
-		try {
+		try 
+		{
 			connection = new Socket(AsxGame.connectionName, AsxGame.portNumber);
 			System.out.println("Local Address: " + connection.getLocalAddress());
 			System.out.println("Local Port: " + connection.getLocalPort());
@@ -106,7 +126,8 @@ public class Game {
     		//Output Streams
 			DeflaterOutputStream deflStream = null;
 			
-			try {
+			try
+			{
 				String emailHash = Integer.toString(uEmail.hashCode());
 				String pwHash = Integer.toString(password.hashCode());
 				
@@ -158,29 +179,44 @@ public class Game {
 					}
 					currentTime = System.currentTimeMillis();
 					elapsedTime = currentTime - startTime;
-					if (elapsedTime > 10000){
+					if (elapsedTime > 10000)
+					{
 						System.out.println("Timeout Error!");
 						successState = false;
 						break;
 					}
 				}
-			} catch (IOException e){
+			}
+			catch (IOException e)
+			{
 				System.out.println("Exception while reading input: " + e);
 				successState = false;
-			} finally {
-				try{
+			}
+			finally
+			{
+				try
+				{
 					connectionRead.close();
-    			} catch (IOException e) {
+    			}
+				catch (IOException e)
+				{
     				System.out.println("Exception while closing streams: " +e);
     			}
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			System.out.println("Exception while opening connection: " + e);
 			successState = false;
-		} finally {
-			try	{
+		}
+		finally
+		{
+			try
+			{
         		connection.close();
-        	} catch (IOException e)	{
+        	}
+			catch (IOException e)
+			{
         		System.out.println("Exception while closing connection: " + e);
         	}
 		}
@@ -190,12 +226,14 @@ public class Game {
 	// register function
 	// registers player, then, if successfull, logs player in using above function
 	// returns TRUE is player successfully registered and logged in, otherwise FALSE
-	protected static boolean registerPlayer(String fName, String sName, String uEmail, String password){
+	protected static boolean registerPlayer(String fName, String sName, String uEmail, String password)
+	{
 		Socket connection = null;
 		boolean successState = false;
 		long startTime = System.currentTimeMillis();
 		long currentTime, elapsedTime;
-		try {
+		try
+		{
 			connection = new Socket(AsxGame.connectionName, AsxGame.portNumber);
 			System.out.println("Local Address: " + connection.getLocalAddress());
 			System.out.println("Local Port: " + connection.getLocalPort());
@@ -207,7 +245,8 @@ public class Game {
     		//Output Streams
 			DeflaterOutputStream deflStream = null;
 			
-			try {
+			try
+			{
 				String pwHash = Integer.toString(password.hashCode());
 				
 				//register call
@@ -242,29 +281,44 @@ public class Game {
 					}
 					currentTime = System.currentTimeMillis();
 					elapsedTime = currentTime - startTime;
-					if (elapsedTime > 10000){
+					if (elapsedTime > 10000)
+					{
 						System.out.println("Timeout Error!");
 						successState = false;
 						break;
 					}
 				}
-			} catch (IOException e){
+			}
+			catch (IOException e)
+			{
 				System.out.println("Exception while reading input: " + e);
 				successState = false;
-			} finally {
-				try{
+			}
+			finally
+			{
+				try
+				{
 					connectionRead.close();
-    			} catch (IOException e) {
+    			}
+				catch (IOException e)
+				{
     				System.out.println("Exception while closing streams: " +e);
     			}
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			System.out.println("Exception while opening connection: " + e);
 			successState = false;
-		} finally {
-			try	{
+		}
+		finally
+		{
+			try
+			{
         		connection.close();
-        	} catch (IOException e)	{
+        	}
+			catch (IOException e)
+			{
         		System.out.println("Exception while closing connection: " + e);
         	}
 		}
@@ -275,13 +329,15 @@ public class Game {
 	// saves the player currently contained in AsxGame.activePlayer
 	// takes a JSONObject of transaction history to append to there file on server
 	// if transHist = null, then doesn't send any
-	protected static boolean saveActivePlayer(JSONObject transHist){
+	protected static boolean saveActivePlayer(JSONObject transHist)
+	{
 		Socket connection = null;
 		boolean successState = false;
 		String saveString;
 		long startTime = System.currentTimeMillis();
 		long currentTime, elapsedTime;
-		try {
+		try
+		{
 			connection = new Socket(AsxGame.connectionName, AsxGame.portNumber);
 			System.out.println("Local Address: " + connection.getLocalAddress());
 			System.out.println("Local Port: " + connection.getLocalPort());
@@ -293,16 +349,20 @@ public class Game {
     		//Output Streams
 			DeflaterOutputStream deflStream = null;
 			
-			try {
+			try
+			{
 				String emailHash = Integer.toString(AsxGame.activePlayer.email.hashCode());
 				
 				//register call
 				deflStream = new DeflaterOutputStream(connection.getOutputStream(), true);
 				System.out.println("Attempt save...");
 				String playerSaveString = AsxGame.activePlayer.generateDataSaveString();
-				if (transHist != null){
+				if (transHist != null)
+				{
 					saveString = "save\n" + emailHash + "\n" + playerSaveString + "\n" + transHist.toString();
-				} else {
+				}
+				else
+				{
 					saveString = "save\n" + emailHash + "\n" + playerSaveString;
 				}
 				System.out.println("Using: " + saveString);
@@ -331,29 +391,44 @@ public class Game {
 					}
 					currentTime = System.currentTimeMillis();
 					elapsedTime = currentTime - startTime;
-					if (elapsedTime > 10000){
+					if (elapsedTime > 10000)
+					{
 						System.out.println("Timeout Error!");
 						successState = false;
 						break;
 					}
 				}
-			} catch (IOException e){
+			}
+			catch (IOException e)
+			{
 				System.out.println("Exception while reading input: " + e);
 				successState = false;
-			} finally {
-				try{
+			}
+			finally
+			{
+				try
+				{
 					connectionRead.close();
-    			} catch (IOException e) {
+    			}
+				catch (IOException e)
+				{
     				System.out.println("Exception while closing streams: " +e);
     			}
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			System.out.println("Exception while opening connection: " + e);
 			successState = false;
-		} finally {
-			try	{
+		}
+		finally
+		{
+			try
+			{
         		connection.close();
-        	} catch (IOException e)	{
+        	}
+			catch (IOException e)
+			{
         		System.out.println("Exception while closing connection: " + e);
         	}
 		}
@@ -362,7 +437,8 @@ public class Game {
 	
 	//this function gets the response from the login request and loads the player into AsxGame.activePlayer
 	//returning true if successful
-	protected static boolean loadPlayer(String response){
+	protected static boolean loadPlayer(String response)
+	{
 		//gets response from login function, gets values for each variable
 		//and creates active player
 		String transHist;
@@ -374,16 +450,22 @@ public class Game {
 		String shareString = json.getString("Shares");
 		String score = json.getString("Score");
 		String rights = json.getString("Rights");
-		try{
+		try
+		{
 			transHist = json.getString("History");
-		} catch (JSONException e) {
+		}
+		catch (JSONException e)
+		{
 			transHist = "";
 		}
-		if (rights.equals("admin")){
+		if (rights.equals("admin"))
+		{
 			AsxGame.activeAdmin = new Player(name,surname,email,balance,shareString,score,rights,transHist);
 			AsxGame.activeAdminLoaded = true;
 			return true;
-		} else {
+		}
+		else
+		{
 			AsxGame.activePlayer = new Player(name,surname,email,balance,shareString,score,rights,transHist);
 			AsxGame.activePlayerLoaded = true;
 			return true;
@@ -392,13 +474,15 @@ public class Game {
 	
 	// loads the leaderboard into AsxGame.leaderBoard
 	// returns true if successful
-	protected static boolean getValueLeaderboard(){
+	protected static boolean getValueLeaderboard()
+	{
 		AsxGame.leaderboard.clear();
 		Socket connection = null;
 		boolean successState = false;
 		long startTime = System.currentTimeMillis();
 		long currentTime, elapsedTime;
-		try {
+		try
+		{
 			connection = new Socket(AsxGame.connectionName, AsxGame.portNumber);
 			System.out.println("Local Address: " + connection.getLocalAddress());
 			System.out.println("Local Port: " + connection.getLocalPort());
@@ -410,7 +494,8 @@ public class Game {
     		//Output Streams
 			DeflaterOutputStream deflStream = null;
 			
-			try {				
+			try
+			{				
 				//leaderboard call
 				deflStream = new DeflaterOutputStream(connection.getOutputStream(), true);
 				String leaderString = "leaders\n" + "0" + "\n" + "10"; //'0' is top position returned, '10' is number of places returned
@@ -429,15 +514,19 @@ public class Game {
 						{
 							System.out.println("Leaderboard download successful!");
 							System.out.println(response);	
-							try {
+							try
+							{
 								String[] leadersJSON = response.split(";");
-								for (int i = 0; i < leadersJSON.length; i++){
+								for (int i = 0; i < leadersJSON.length; i++)
+								{
 									JSONObject json = new JSONObject(leadersJSON[i]);
 									AsxGame.leaderboard.add(json);
 								}
 								successState = true;
 								break;
-							} catch (JSONException e){
+							}
+							catch (JSONException e)
+							{
 								e.printStackTrace();
 								successState = false;
 								break;
@@ -451,29 +540,44 @@ public class Game {
 					}
 					currentTime = System.currentTimeMillis();
 					elapsedTime = currentTime - startTime;
-					if (elapsedTime > 10000){
+					if (elapsedTime > 10000)
+					{
 						System.out.println("Timeout Error!");
 						successState = false;
 						break;
 					}
 				}
-			} catch (IOException e){
+			}
+			catch (IOException e)
+			{
 				System.out.println("Exception while reading input: " + e);
 				successState = false;
-			} finally {
-				try{
+			}
+			finally
+			{
+				try
+				{
 					connectionRead.close();
-    			} catch (IOException e) {
+    			}
+				catch (IOException e)
+				{
     				System.out.println("Exception while closing streams: " +e);
     			}
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			System.out.println("Exception while opening connection: " + e);
 			successState = false;
-		} finally {
-			try	{
+		}
+		finally
+		{
+			try
+			{
         		connection.close();
-        	} catch (IOException e)	{
+        	}
+			catch (IOException e)
+			{
         		System.out.println("Exception while closing connection: " + e);
         	}
 		}
@@ -482,9 +586,12 @@ public class Game {
 	
 	// this function returns the current price for a specific stock
 	// returns -1 if it cant find a stock
-	public static float getStockCurrentPrice(String asxCode){
-		for (int i = 0; i < AsxGame.stockArray.size(); i++){
-			if (AsxGame.stockArray.get(i).code.equals(asxCode)){
+	public static float getStockCurrentPrice(String asxCode)
+	{
+		for (int i = 0; i < AsxGame.stockArray.size(); i++)
+		{
+			if (AsxGame.stockArray.get(i).code.equals(asxCode))
+			{
 				return AsxGame.stockArray.get(i).askPrice;
 			}
 		}
@@ -561,14 +668,16 @@ public class Game {
 	
 	//these 2 used to calculate the brokers fee for buying and selling
 	// both return floats
-	public static float calcBrokersFeePurch(float transactionAmount){ 	//$50 + %1
+	public static float calcBrokersFeePurch(float transactionAmount)
+	{ 	//$50 + %1
 		float output = 50;
 		float fee = (float) (transactionAmount * 0.01);
 		output += fee;
 		return output;
 	}
 	
-	public static float calcBrokersFeeSale(float transactionAmount){ 	//$50 + %1
+	public static float calcBrokersFeeSale(float transactionAmount)
+	{ 	//$50 + %1
 		float output = 50;
 		float fee = (float) (transactionAmount * 0.0025);
 		output += fee;
